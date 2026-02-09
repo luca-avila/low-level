18232be504b0dff68f2cbfe8f3e0834e47cf57d5
luca-avila <avilaluca61@gmail.com>
2026-02-09 13:06:42 -0300

    Compare type sizes

---
+++ basics/types/sizes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Int size: %zu\n", sizeof(int));
+    printf("Char size: %zu\n", sizeof(char));
+    printf("Double size: %zu\n", sizeof(double));
+    printf("Float size: %zu\n", sizeof(float));
+    return 0;
+}
